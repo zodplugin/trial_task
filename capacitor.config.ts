305aacc8b1453934cc34187cbfeaaf20e080f9f4
zodplugin <zodplugin@gmail.com>
2024-12-03 19:44:40 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trial_task',
+  webDir: 'www'
+};
+
+export default config;
